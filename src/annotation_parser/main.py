560afce2e8afdd3482985b5d3685c45407ee6ff1
yusha <pratyusha.gh@gmail.com>
2026-05-16 17:47:09 +0530

    chore: add build parser to main

--- src/annotation_parser/main.py
+++ src/annotation_parser/main.py
@@ -1,5 +1,16 @@
+from src.annotation_parser.cli_args import build_parser
+
+
 def main() -> None:
-    print("main function")
+    args = build_parser()
+    print(args)
+
+    # TODO:
+    # 1. Read clipping file
+    # 2. Parse annotations
+    # 3. Group into books
+    # 4. Merge notes
+    # 5. Export markdown
 
 
 if __name__ == "__main__":
